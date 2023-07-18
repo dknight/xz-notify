fd3cf99427e4186d1c77f288d71cff67c8e48eb2
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2023-07-18 11:35:44 +0300

    feat: change defaul theme

--- __tests__/xz-notify.test.ts
+++ __tests__/xz-notify.test.ts
@@ -1,6 +1,6 @@
 import {jest} from '@jest/globals';
 import {SpiedFunction} from 'jest-mock';
-import XZNotify from '../src/xz-notify';
+import XZNotify from '../index';
 
 describe('Should test', () => {
   let component: XZNotify;
